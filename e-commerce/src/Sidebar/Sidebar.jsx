319e5718aa06e85005f987b13c0e1253ab6d923c
Alpy81 <alperen.ertas@gmx.de>
2023-12-21 00:23:16 +0100

    update commit

--- e-commerce/src/Sidebar/Sidebar.jsx
+++ e-commerce/src/Sidebar/Sidebar.jsx
@@ -5,17 +5,15 @@ import Category from "./Category/Category";
 import Price from "./Price/Price";
 import Colors from "./Colors/Colors";
 
-const Sidebar = () => {
+const Sidebar = ({ handleChange }) => {
   return (
     <section className="sidebar">
       <div className="logo-container">
-        <h1>
-          <BsCart4 />
-        </h1>
+        <h1>🛒</h1>
       </div>
-      <Category />
-      <Price />
-      <Colors />
+      <Category handleChange={handleChange} />
+      <Price handleChange={handleChange} />
+      <Colors handleChange={handleChange} />
     </section>
   );
 };
